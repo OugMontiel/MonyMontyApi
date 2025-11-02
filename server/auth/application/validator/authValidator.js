eb0e4501ee73411ea84a05373eefe3d8ab751c5e
PcGamer <gogoedy@hotmail.com>
2025-11-01 20:25:53 -0500

    inicio de la adecuacion ..
    router y validadores

--- server/auth/application/validator/authValidator.js
+++ server/auth/application/validator/authValidator.js
@@ -1,4 +1,4 @@
-const {body} = require("express-validator");
+const {body, query} = require("express-validator");
 
 const Validador = require("../../../core/validador/Validador.js");
 class AuthValidator {
@@ -15,6 +15,38 @@ class AuthValidator {
       body("email").notEmpty().withMessage("send a email").isEmail().withMessage("Please enter a valid email address"),
 
       Validador.noQueryParams(),
+    ];
+  };
+  validatorEmail = () => {
+    return [
+      // Validación de email
+      body("email").notEmpty().withMessage("send a email").isEmail().withMessage("Please enter a valid email address"),
+
+      Validador.noQueryParams(),
+    ];
+  };
+  validatorPassword = () => {
+    return [
+      // Validación de password
+      body("password")
+        .notEmpty()
+        .withMessage("send a password")
+        .isLength({min: 8})
+        .withMessage("password must be at least 8 characters long"),
+
+      Validador.noQueryParams(),
+    ];
+  };
+  checkToken = () => {
+    return [
+      // Validación de token
+      query("token")
+        .notEmpty()
+        .withMessage("send a token")
+        .isString()
+        .withMessage("token must be a string"),
+
+      Validador.noBodyData(),
     ];
   };
 }
