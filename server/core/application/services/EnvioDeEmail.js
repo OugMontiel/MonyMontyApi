4f525928ed3d4930c2c6efe4b0e34f9345918a12
PcGamer <gogoedy@hotmail.com>
2025-11-05 17:50:07 -0500

    Correcto nombre de variable de entorno

--- server/core/application/services/EnvioDeEmail.js
+++ server/core/application/services/EnvioDeEmail.js
@@ -1,6 +1,6 @@
 const { Resend } = require('resend');
 
-const resend = new Resend(process.env.EMAIL_APY_KEY);
+const resend = new Resend(process.env.EMAIL_API_KEY);
 
 const enviosDeCorreo = async(alias,tos,subject, html) => {
 
