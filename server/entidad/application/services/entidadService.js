db8c80e4b470dfed6d775a986ceba1897a7f38c9
PcGamer <gogoedy@hotmail.com>
2026-01-14 22:17:42 -0500

    Creacion Incial del Documento Validacion modelo y servicios

--- server/entidad/application/services/entidadService.js
+++ server/entidad/application/services/entidadService.js
@@ -1,3 +1,4 @@
+const {ObjectId} = require("mongodb");
 const entidadRepository = require("../../domain/repositories/entidadRepository");
 
 class EntidadService {
@@ -13,6 +14,33 @@ class EntidadService {
       throw {
         status: 500,
         message: "Error interno al obtener las entidades",
+      };
+    }
+  }
+
+  async crear(data, usuario) {
+    try {
+      const fechaActual = new Date();
+
+      const nuevaEntidad = {
+        ...data,
+        usuarioId: new ObjectId(usuario._id),
+        createdAt: fechaActual,
+        updatedAt: fechaActual,
+        auditoria: {
+          creadoPor: {
+            usuarioId: new ObjectId(usuario._id),
+            nombre: usuario.nombre,
+          },
+        },
+      };
+
+      return await this.entidadRepository.crear(nuevaEntidad);
+    } catch (error) {
+      console.error("Error en servicio - crear entidad:", error);
+      throw {
+        status: error.status || 500,
+        message: error.message || "Error interno al crear la entidad",
       };
     }
   }
--- server/entidad/application/validator/entidadValidator.js
+++ server/entidad/application/validator/entidadValidator.js
@@ -4,6 +4,15 @@ class EntidadValidator {
   noBodyNoQuery() {
     return [validador.noBodyData(), validador.noQueryParams()];
   }
+
+  validarCreacion() {
+    return [
+      validador.requiredString("nombre"),
+      validador.requiredString("tipo"),
+      validador.requiredNumber("saldoInicial"),
+      validador.requiredString("divisaId"),
+    ];
+  }
 }
 
 module.exports = new EntidadValidator();
